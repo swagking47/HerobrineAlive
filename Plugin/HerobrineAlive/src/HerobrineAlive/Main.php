e03f3d4ae6eea90c8e7172a35f3dc049fbb284bd
Philip Shilling <comedyman937@yahoo.com>
2014-06-09 06:25:52 -0700

    Added Checks For Herobrine Active

--- Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
+++ Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
@@ -10,6 +10,7 @@ use pocketmine\event\Listener;
 use pocketmine\Player;
 
 class Main extends PluginBase implements Listener, CommandExecutor{
+    $HerobrineActive = false;
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->log("[INFO] HerobrineAlive Loaded!");
@@ -19,25 +20,39 @@ class Main extends PluginBase implements Listener, CommandExecutor{
         switch($cmd->getName()){
             case "herobrinealive":
                 if($args[0] == "release"){
-                    $this->getServer()->getScheduler()->scheduleTask(new StartHerobrineAI($this), 1);
-                    $sender->sendMessage("[HerobrineAlive] Herobrine has been released!");
+                    if($HerobrineActive == false){
+                        $this->getServer()->getScheduler()->scheduleTask(new StartHerobrineAI($this), 1);
+                        $HerobrineActive = true;
+                        $sender->sendMessage("[HerobrineAlive] Herobrine has been released!");
+                    }else{
+                    	$sender->sendMessage("[HerobrineAlive] Herobrine is already active!");
+                    }
                 }elseif($args[0] == "kill"){
-                    $this->getServer()->getScheduler()->scheduleTask(new StopHerobrineAI($this), 1);
-                    $sender->sendMessage("[HerobrineAlive] Herobrine has been killed!");
+                    if($HerobrineActive == true){
+                        $this->getServer()->getScheduler()->scheduleTask(new StopHerobrineAI($this), 1);
+                        $HerobrineActive = false;
+                        $sender->sendMessage("[HerobrineAlive] Herobrine has been killed!");
+                    }else{
+                    	$sender->sendMessage("[HerobrineAlive] Herobrine isn't currently active!");
+                    }
                 }elseif($args[0] == "attack"){
                     if($args[1] == instance of player){
-                        $xd=/*cos(deg2rad(floatval($this->p->entity->yaw)))*/1*$this->distance;
-		                $zd=/*sin(deg2rad(floatval($this->p->entity->pitch)))*/0*$this->distance;
-		                $pk=new MovePlayerPacket;
-	                	$pk->eid=$this->eid;
-		                $pk->x=$this->p->entity->x+$xd;
-		                $pk->y=$this->p->entity->y;
-		                $pk->z=$this->p->entity->z+$zd;
-		                $pk->bodyYaw=$this->p->entity->yaw;
-		                $pk->yaw=$this->p->entity->yaw;
-		                $pk->pitch=$this->p->entity->pitch;
-		                $this->p->dataPacket($pk);
-                        $sender->sendMessage("[HerobrineAlive] Herobrine is now after " . $args[1] . "!");
+                    	if($HerobrineActive == true){
+                            $xd=/*cos(deg2rad(floatval($this->p->entity->yaw)))*/1*$this->distance;
+		            $zd=/*sin(deg2rad(floatval($this->p->entity->pitch)))*/0*$this->distance;
+		            $pk=new MovePlayerPacket;
+	                    $pk->eid=$this->eid;
+		            $pk->x=$this->p->entity->x+$xd;
+		            $pk->y=$this->p->entity->y;
+		            $pk->z=$this->p->entity->z+$zd;
+		            $pk->bodyYaw=$this->p->entity->yaw;
+		            $pk->yaw=$this->p->entity->yaw;
+		            $pk->pitch=$this->p->entity->pitch;
+		            $this->p->dataPacket($pk);
+                            $sender->sendMessage("[HerobrineAlive] Herobrine is now after " . $args[1] . "!");
+                    	}else{
+                    	    $sender->sendMessage("[HerobrineAlive] Herobrine must be active to attack!");
+                    	}
                     }else{
                         $sender->sendMessage("[HerobrineAlive] Target player not found!");
                     }
