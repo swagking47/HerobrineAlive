f226e68adda58f1da56a44add606274f43d11105
Philip Shilling <comedyman937@yahoo.com>
2014-06-09 05:58:41 -0700

    Start Packet create (Thanks @PEMapModder)

--- Plugin/HerobrineAlive/src/HerobrineAlive/StartHerobrineAI.php
+++ Plugin/HerobrineAlive/src/HerobrineAlive/StartHerobrineAI.php
@@ -6,9 +6,33 @@ use pocketmine\scheduler\PluginTask;
 use pocketmine\Player;
 
 class StartHerobrineAI extends PluginTask{
+    public $p;
+	protected $distance = 5, $settings; // settings
+	protected $eid:
     public function onRun(){
-        /*
-         * Create Herobrine
-         */
+        $xcord = rand(1, 256);
+        $ycord = rand(1, 256);
+        $zcord = rand(1, 256);
+        $xd = /*cos(deg2rad(floatval($this->p->entity->yaw)))*/1*$this->distance;
+		$zd = /*sin(deg2rad(floatval($this->p->entity->pitch)))*/0*$this->distance;
+		$pk = new AddPlayerPacket;
+		$pk->clientId = 0;
+		$this->username = "Herobrine"
+		$pk->username = $this->username;
+		$pk->eid = $this->eid;
+		$pk->x = $this->p->entity->x+$xd;
+		$pk->y = $this->p->entity->y;
+		$pk->z = $this->p->entity->z+$zd;
+		$pk->yaw = 180+$this->p->yaw;
+		$pk->pitch = 180+$this->p->pitch;
+		$pk->unknown1 = 0;
+		$pk->unknown2 = 0;
+		$pk->metadata = array(
+				0 => array("type" => 0, "value" => 0b00),
+				1 => array("type" => 1, "value" => 300),
+				16 => array("type" => 0, "value" => 0),
+				17 => array("type" => 6, "value" => array(0, 0, 0))
+		);
+		$this->p->dataPacket($pk);
     }
 }
