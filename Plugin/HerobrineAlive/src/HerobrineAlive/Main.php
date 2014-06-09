156c0498cbc3b8601f5df8e34a51b901a990e4be
Philip Shilling <comedyman937@yahoo.com>
2014-06-09 06:14:10 -0700

    Start Attack Packet (Thanks @PEMapModder)

--- Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
+++ Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
@@ -26,7 +26,17 @@ class Main extends PluginBase implements Listener, CommandExecutor{
                     $sender->sendMessage("[HerobrineAlive] Herobrine has been killed!");
                 }elseif($args[0] == "attack"){
                     if($args[1] == instance of player){
-                        //Have Herobrine go after $args[1]
+                        $xd=/*cos(deg2rad(floatval($this->p->entity->yaw)))*/1*$this->distance;
+		                $zd=/*sin(deg2rad(floatval($this->p->entity->pitch)))*/0*$this->distance;
+		                $pk=new MovePlayerPacket;
+	                	$pk->eid=$this->eid;
+		                $pk->x=$this->p->entity->x+$xd;
+		                $pk->y=$this->p->entity->y;
+		                $pk->z=$this->p->entity->z+$zd;
+		                $pk->bodyYaw=$this->p->entity->yaw;
+		                $pk->yaw=$this->p->entity->yaw;
+		                $pk->pitch=$this->p->entity->pitch;
+		                $this->p->dataPacket($pk);
                         $sender->sendMessage("[HerobrineAlive] Herobrine is now after " . $args[1] . "!");
                     }else{
                         $sender->sendMessage("[HerobrineAlive] Target player not found!");
