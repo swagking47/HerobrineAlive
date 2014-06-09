4c1b5057bbc8e5c6f46beee1678973191c60af9b
Philip Shilling <comedyman937@yahoo.com>
2014-06-09 06:29:09 -0700

    Update Main.php

--- Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
+++ Plugin/HerobrineAlive/src/HerobrineAlive/Main.php
@@ -38,16 +38,16 @@ class Main extends PluginBase implements Listener, CommandExecutor{
                 }elseif($args[0] == "attack"){
                     if($args[1] == instance of player){
                     	if($HerobrineActive == true){
-                            $xd=/*cos(deg2rad(floatval($this->p->entity->yaw)))*/1*$this->distance;
-		            $zd=/*sin(deg2rad(floatval($this->p->entity->pitch)))*/0*$this->distance;
-		            $pk=new MovePlayerPacket;
-	                    $pk->eid=$this->eid;
-		            $pk->x=$this->p->entity->x+$xd;
-		            $pk->y=$this->p->entity->y;
-		            $pk->z=$this->p->entity->z+$zd;
-		            $pk->bodyYaw=$this->p->entity->yaw;
-		            $pk->yaw=$this->p->entity->yaw;
-		            $pk->pitch=$this->p->entity->pitch;
+                            $xd = /*cos(deg2rad(floatval($this->p->entity->yaw)))*/1*$this->distance;
+		            $zd = /*sin(deg2rad(floatval($this->p->entity->pitch)))*/0*$this->distance;
+		            $pk = new MovePlayerPacket;
+	                    $pk->eid = $this->eid;
+		            $pk->x = $this->p->entity->x+$xd;
+		            $pk->y = $this->p->entity->y;
+		            $pk->z = $this->p->entity->z+$zd;
+		            $pk->bodyYaw = $this->p->entity->yaw;
+		            $pk->yaw = $this->p->entity->yaw;
+		            $pk->pitch = $this->p->entity->pitch;
 		            $this->p->dataPacket($pk);
                             $sender->sendMessage("[HerobrineAlive] Herobrine is now after " . $args[1] . "!");
                     	}else{
